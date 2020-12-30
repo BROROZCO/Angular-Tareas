dac7290b01736c97dceb9fcd83b5fd23f7e50b71
unknown <unenes.viejos@gmail.com>
2020-12-30 10:23:54 -0600

    Creacion de componentes navbar y tareas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { TareasComponent } from './Components/tareas/tareas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TareasComponent
   ],
   imports: [
     BrowserModule
